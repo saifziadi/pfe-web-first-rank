b75f9729294406921733f7c0002c7470a3bfc6ec
Yassine El Ouni <yelouni@naxxum.fr>
2020-07-09 01:01:22 +0100

    adding buy button

--- CLIENT/src/app/layouts/public/home/tools/tools-list/tools-list.component.ts
+++ CLIENT/src/app/layouts/public/home/tools/tools-list/tools-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'app/services/account.service';
+import { ToolService } from 'app/services/Tool.service';
 
 @Component({
   selector: 'app-tools-list',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolsListComponent implements OnInit {
 
-  constructor() { }
+  tools: any = []
+  groupedTools : any = []
+
+  constructor(private accountService: AccountService,
+    private toolService: ToolService,) {
+      this.GroupedByCategory()
+     }
 
   ngOnInit(): void {
   }
 
+  GroupedByCategory() {
+    this.toolService.GroupedByCategory()
+      .subscribe((res: any) => {
+        console.log("res tools : ", res)
+
+        this.tools = res
+        if (this.tools && this.tools.length>0) {
+          this.tools[0].active = true
+          this.groupedTools = this.tools[0].tools
+          console.log("this.groupedTools : ",this.groupedTools)
+
+        }
+      })
+  }
+
+  activateCategory(id){
+    this.tools.map(x=>{
+      if (x._id != id && x.active) {
+        x.active = false
+      }else if (x._id ==id){
+        x.active = true
+        this.groupedTools = this.tools.find(x=>x._id == id).tools
+        console.log("this.groupedTools : ",this.groupedTools)
+      }
+    })
+  }
 }
